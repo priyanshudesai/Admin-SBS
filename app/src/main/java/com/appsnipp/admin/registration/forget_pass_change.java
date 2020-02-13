8db6620fc93f84fd87e4787495e2de9ed40c9bb7
kaushal-king <kaushaljethava12@gmail.com>
2020-02-13 16:57:51 +0530

    otp

---
+++ app/src/main/java/com/appsnipp/admin/registration/forget_pass_change.java
@@ -0,0 +1,82 @@
+package com.appsnipp.admin.registration;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.appsnipp.admin.R;
+import com.appsnipp.admin.apiinterface.Api;
+import com.appsnipp.admin.apiinterface.ApiClient;
+import com.appsnipp.admin.apiinterface.CommanResponse;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class forget_pass_change extends AppCompatActivity {
+    EditText password, conformpassword;
+//    TextView tosttext1;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forget_pass_change);
+        password = (EditText) findViewById(R.id.pass1);
+        conformpassword = (EditText) findViewById(R.id.cpass);
+//        tosttext1=(TextView) findViewById(R.id.toasttext);
+    }
+
+    public void regiback(View view) {
+        super.onBackPressed();
+
+    }
+
+    public void checkpassmatchp(View view) {
+        String s1 = password.getText().toString();
+        String s2 = conformpassword.getText().toString();
+        boolean k = true;
+        if (s1.isEmpty() && s1.length() < 8) {
+            password.setError(" 8 character necessary");
+            k = false;
+        }
+        if (s2.isEmpty()) {
+            conformpassword.setError("Invailid Conform-Password");
+            k = false;
+        }
+        if (!s2.equals(s1)) {
+            conformpassword.setError("Not Match");
+            k = false;
+        }
+        if (k == true) {
+//            tosttext1.setText("match passeord");
+//            LayoutInflater li = getLayoutInflater();
+//            View layout = li.inflate(R.layout.toast,(ViewGroup) findViewById(R.id.toast));
+//            Toast t=new Toast(getApplicationContext());
+//            t.setDuration(Toast.LENGTH_SHORT);
+//
+//            t.setView(layout);
+//            t.show();
+            Intent i1 = getIntent();
+            String mob = i1.getStringExtra("mob");
+            Api api = ApiClient.getClient().create(Api.class);
+            Call<CommanResponse> call=api.changepass("changepass",mob,s1);
+            call.enqueue(new Callback<CommanResponse>() {
+                @Override
+                public void onResponse(Call<CommanResponse> call, Response<CommanResponse> response) {
+                    Toast.makeText(forget_pass_change.this, response.body().getMessage()+"", Toast.LENGTH_SHORT).show();
+
+                }
+
+                @Override
+                public void onFailure(Call<CommanResponse> call, Throwable t) {
+                    Toast.makeText(forget_pass_change.this, t.getLocalizedMessage()+"", Toast.LENGTH_SHORT).show();
+
+                }
+            });
+
+
+        }
+    }
+}
