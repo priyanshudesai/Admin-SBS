6bdd7f28c52052bb598c1a0181f5170f4d1eb47c
kaushal-king <kaushaljethava12@gmail.com>
2020-02-13 16:08:34 +0530

    otp

--- app/src/main/java/com/appsnipp/admin/registration/Forgotpassword_form.java
+++ app/src/main/java/com/appsnipp/admin/registration/Forgotpassword_form.java
@@ -1,6 +1,7 @@
 package com.appsnipp.admin.registration;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
@@ -15,10 +16,22 @@ import android.widget.Toast;
 
 import com.appsnipp.admin.LoginActivity;
 import com.appsnipp.admin.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
 
 public class Forgotpassword_form extends AppCompatActivity implements TextWatcher {
-EditText e1,e2,e3,e4,mono;
-KeyEvent k;
+    EditText e1,e2,e3,e4,e5,e6,mono;
+    KeyEvent k;
+    FirebaseAuth auth;
+    PhoneAuthProvider.OnVerificationStateChangedCallbacks mcallback;
+    String veri_code;
 //TextView tosttext1;
 
     @Override
@@ -29,13 +42,61 @@ KeyEvent k;
         e2= findViewById(R.id.editText_two);
         e3= findViewById(R.id.editTex_three);
         e4= findViewById(R.id.editTex_four);
+        e5= findViewById(R.id.editTex_five);
+        e6= findViewById(R.id.editTex_six);
         mono=(EditText) findViewById(R.id.forgotmobile);
         e1.addTextChangedListener(this);
         e2.addTextChangedListener(this);
         e3.addTextChangedListener(this);
         e4.addTextChangedListener(this);
+        e5.addTextChangedListener(this);
+        e6.addTextChangedListener(this);
 //       tosttext1=(TextView) findViewById(R.id.toasttext);
         mono.requestFocus();
+        auth= FirebaseAuth.getInstance();
+
+        mcallback= new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+            @Override
+            public void onVerificationCompleted(PhoneAuthCredential phoneAuthCredential) {
+
+            }
+
+            @Override
+            public void onVerificationFailed(FirebaseException e) {
+
+            }
+
+            @Override
+            public void onCodeSent(String s, PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                super.onCodeSent(s, forceResendingToken);
+                veri_code=s;
+                Toast.makeText(Forgotpassword_form.this, "code Sent", Toast.LENGTH_SHORT).show();
+            }
+
+        };
+    }
+    public  void send_sms(View v)
+    {   String m=mono.getText().toString().trim();
+        PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                m,60, TimeUnit.SECONDS,this,mcallback
+
+        );
+    }
+
+    public void changepa(PhoneAuthCredential credential)
+    {
+        auth.signInWithCredential(credential)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful())
+                        {
+                            Toast.makeText(Forgotpassword_form.this, "otp match", Toast.LENGTH_SHORT).show();
+                            Intent i = new Intent(Forgotpassword_form.this, password.class);
+                            startActivity(i);
+                        }
+                    }
+                });
     }
 
     public void loginback2(View view) {
@@ -45,27 +106,38 @@ KeyEvent k;
     }
 
     public void password(View view) {
-        boolean v=true;
-        String s3=mono.getText().toString();
-        String MobilePattern = "[0-9]{10}";
-        if (s3.isEmpty() || !s3.matches(MobilePattern)){
+//        boolean v=true;
+//        String s3=mono.getText().toString();
+//        String MobilePattern = "[0-9]{10}";
+//        if (s3.isEmpty() || !s3.matches(MobilePattern)){
+//
+//            mono.setError("Invailid");
+//            v=false;
+//        }
+//        if(v==true) {
+//            Intent i = new Intent(Forgotpassword_form.this, password.class);
+//            startActivity(i);
+//        }
+//        else {
+////           tosttext1.setText("match passeord");
+//            LayoutInflater li = getLayoutInflater();
+//            View layout = li.inflate(R.layout.toast,(ViewGroup) findViewById(R.id.toast));
+//            Toast t=new Toast(getApplicationContext());
+//            t.setDuration(Toast.LENGTH_SHORT);
+//            t.setView(layout);
+//            t.show();
+//        }
 
-            mono.setError("Invailid");
-            v=false;
+        String code=""+e1.getText()+e2.getText()+e3.getText()+e4.getText()+e5.getText()+e6.getText();
+        if(code.length()!=0)
+        {
+            veri_phone(veri_code,code);
         }
-        if(v==true) {
-            Intent i = new Intent(Forgotpassword_form.this, password.class);
-            startActivity(i);
-        }
-        else {
-//           tosttext1.setText("match passeord");
-            LayoutInflater li = getLayoutInflater();
-            View layout = li.inflate(R.layout.toast,(ViewGroup) findViewById(R.id.toast));
-            Toast t=new Toast(getApplicationContext());
-            t.setDuration(Toast.LENGTH_SHORT);
-            t.setView(layout);
-            t.show();
-        }
+    }
+
+    private void veri_phone(String veri_code, String code) {
+        PhoneAuthCredential credential= PhoneAuthProvider.getCredential(veri_code,code);
+        changepa(credential);
     }
 
     public void regiback(View view) {
@@ -112,7 +184,7 @@ KeyEvent k;
 //            }
 //
 //
-      }
+        }
 
     }
 
@@ -122,21 +194,21 @@ KeyEvent k;
             mono.requestFocus();
         }
 
-            if (e4.length() == 0) {
-                if (keyCode == event.KEYCODE_DEL) {
-                    e3.requestFocus();
-                }
+        if (e4.length() == 0) {
+            if (keyCode == event.KEYCODE_DEL) {
+                e3.requestFocus();
             }
-            if (e3.length() == 0) {
-                if (keyCode == event.KEYCODE_DEL) {
-                    e2.requestFocus();
-                }
+        }
+        if (e3.length() == 0) {
+            if (keyCode == event.KEYCODE_DEL) {
+                e2.requestFocus();
             }
-            if (e2.length() == 0) {
-                if (keyCode == event.KEYCODE_DEL) {
-                    e1.requestFocus();
-                }
+        }
+        if (e2.length() == 0) {
+            if (keyCode == event.KEYCODE_DEL) {
+                e1.requestFocus();
             }
+        }
 
         return super.onKeyDown(keyCode, event);
     }
